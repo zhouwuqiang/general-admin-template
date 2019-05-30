ccdbff34b8e200338fcb641071ac1e958152d46e
root <>
2019-05-30 11:01:44 +0800

    调整mvc结构

---
+++ src/main/java/com/java/general/config/web/WebMvcConfigurer.java
@@ -0,0 +1,34 @@
+package com.java.general.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * description :
+ *
+ * @author alger
+ * @version 1.0.0
+ * @date 2019/3/6 13:02
+ */
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+
+
+}
